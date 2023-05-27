11a506cf74771357ca871ccb981bed4a4124372f
Elin Forsberg <elinfsp96@hotmail.com>
2023-05-27 10:58:36 +0200

    routes forts

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -3,7 +3,7 @@ import LoginForm from "../LoginForm/LoginForm";
 import "./Login.css"
 // import {Drawer, Button} from "@mui/material";
 
-export default function Login() 
+function Login() 
 {
     return(
         <div  className="loginContainer">
@@ -14,24 +14,5 @@ export default function Login()
     )
     
 }
-// export default function LoginDrawer({open, setOpen}) {
-//     const toggleDrawer = () => (event) => {
-//         if (
-//             event.type === "keydown" &&
-//             (event.key === "Tab" || event.key === "Shift")
-//         ) {
-//             return;
-//         }
-//         setOpen(false);
-//     };
-
-//     return (
-//         <Drawer anchor={top} open={open} onClose={toggleDrawer()}>
-//             <div  className="loginContainer">
-// //          <LoginForm />
-// //         </div>
-//         </Drawer>
-//     )
-// }
-
+export default Login
 
