11973a8d1934659113f76af85fca65a0a47d14e4
isabellapresto <isabella.presto@gmail.com>
2023-05-30 14:23:24 +0200

    Product details

--- client/src/components/ProductDetails/ProductDetails.tsx
+++ client/src/components/ProductDetails/ProductDetails.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import ProductCard from '../ProductCard/ProductCard';
 import Product from '../../interfaces';
+import { useParams } from "react-router-dom";
 
 
 export default function ProductDetails() {
-  const [products, setDetails] = useState<Product[]>([]);
-  
+  const [product, setProduct] = useState(null);
+  const { _id } = useParams();
+
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-        const apiUrl = "http://localhost:3000/api/products/${id}";
+        const apiUrl = `http://localhost:3000/api/products/${_id}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
-        setDetails(data);
+        setProduct(data);
+        console.log(data)
       } catch (error) {
         console.log(error);
       }
@@ -21,16 +24,26 @@ export default function ProductDetails() {
     fetchDetails();
   }, []);
 
-  return (
-    <>
-      {products.map(product => {
-        return <ProductCard 
-          image={product.image}
-          title={product.title}
-          price={product.price}
-          description={product.description}
-          /> 
-        })}
-    </>
+  return   (
+
+
+    // <>
+    //   {products.map(product => {
+    //     return <ProductCard 
+    //       image={product.image}
+    //       title={product.title}
+    //       price={product.price}
+    //       description={product.description}
+    //       /> 
+    //     })}
+    // </>
+
+<div>
+<h1>{product.title}</h1>
+<p>{product.description}</p> 
+</div>
+
+
+
   );
 }
