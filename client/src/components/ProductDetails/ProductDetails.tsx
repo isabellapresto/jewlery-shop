ca6e4db4963f13ee86ff6c242ec3d9528f6e20ad
isabellapresto <isabella.presto@gmail.com>
2023-05-30 13:26:53 +0200

    Product details

--- client/src/components/ProductDetails/ProductDetails.tsx
+++ client/src/components/ProductDetails/ProductDetails.tsx
@@ -1,42 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import ProductModel from "../Models/productModel";
+import { useEffect, useState } from "react";
+import ProductCard from '../ProductCard/ProductCard';
+import Product from '../../interfaces';
+
 
 export default function ProductDetails() {
-  const { id } = useParams<{ id: string }>();
-  const [product, setProduct] = useState<ProductModel | null>(null);
-
+  const [products, setDetails] = useState<Product[]>([]);
+  
   useEffect(() => {
-    const fetchProduct = async () => {
+    const fetchDetails = async () => {
       try {
-        const apiUrl = `http://localhost:3000/api/products/${id}`;
+        const apiUrl = "http://localhost:3000/api/products/${id}";
         const response = await fetch(apiUrl);
         const data = await response.json();
-        setProduct(data);
+        setDetails(data);
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchProduct();
-  }, [id]);
+    fetchDetails();
+  }, []);
 
   return (
-    <div>
-      {product ? (
-        <div>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <p>Price: {product.price}</p>
-          <img
-            src={product.image}
-            alt={product.title}
-            style={{ maxWidth: "300px", maxHeight: "300px" }}
-          />
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      {products.map(product => {
+        return <ProductCard 
+          image={product.image}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          /> 
+        })}
+    </>
   );
 }
