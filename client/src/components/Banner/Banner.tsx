a8447add910cf07b6cad4a8b676f385690d18ca5
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-05-31 21:23:14 +0200

    functionality for the banner buttons

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -1,6 +1,7 @@
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import '../Banner/Banner.css'
+import { useState } from 'react';
 
 const images = [
   'https://lilyandrose.se/wp-content/uploads/2023/04/statement-gift-guide-kategori-1000x417.jpg',
@@ -8,14 +9,32 @@ const images = [
   'https://lilyandrose.se/wp-content/uploads/2023/05/kategoribanner-spring-clearance-1000x417.jpg',
 ]
 
-
 export default function Banner() {
+
+  const [index, setIndex] = useState(0)
+
+  function nextStep() {
+    if(index === images.length -1) {
+      setIndex(0)
+      return
+    }
+    setIndex(index + 1)
+  }
+
+  function prevStep() {
+    if (index === 0) {
+      setIndex(images.length - 1)
+      return
+    }
+    setIndex(index - 1)
+  }
+
     return (
       <div className="container">
         <div className="slideshow">
-          <img src={images[0]} alt="slides" className="slides" />
-          <button className="prevBtn"><ArrowBackIosIcon /></button>
-          <button className="nextBtn"><ArrowForwardIosIcon /></button>
+          <img src={images[index]} alt="slides" className="slides" />
+          <button className="prevBtn" onClick={prevStep}><ArrowBackIosIcon /></button>
+          <button className="nextBtn" onClick={nextStep}><ArrowForwardIosIcon /></button>
         </div>
       </div>
     )
