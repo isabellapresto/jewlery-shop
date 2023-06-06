cbe4163fd374a499d833de9064f9bb00cbd2009f
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-06-06 12:34:59 +0200

    CSS navlinks

--- client/src/components/NavLinks/NavLinks.tsx
+++ client/src/components/NavLinks/NavLinks.tsx
@@ -10,37 +10,38 @@ import {
 import "./NavLinks.css";
 
 function NavLinks() {
+
   return (
     <List
       component="nav"
       aria-labelledby="nav-links"
-      sx={{ display: "flex", justifyContent: "center", gap: "35px" }}
+      sx={{ display: "flex", justifyContent: "center", gap: "35px"}}
     >
-      <NavLink to="/" style={{ textDecoration: "none" }}>
-        <Button component={ListItem} disableRipple>
+      <NavLink to="/" style={{textDecoration: "none" }}>
+        <Button className="active menu-hover-effect" component={ListItem} disableRipple>
           <ListItemText
-            primary={<Typography variant="body1">HOME</Typography>}
+            primary={<Typography className="text-menu" variant="body1">HOME</Typography>}
           />
         </Button>
       </NavLink>
       <NavLink to="/shop" style={{ textDecoration: "none" }}>
-        <Button component={ListItem} disableRipple>
+        <Button className="menu-hover-effect" component={ListItem} disableRipple>
           <ListItemText
-            primary={<Typography variant="body1">SHOP</Typography>}
+            primary={<Typography className="text-menu" variant="body1">SHOP</Typography>}
           />
         </Button>
       </NavLink>
       <NavLink to="/about" style={{ textDecoration: "none" }}>
-        <Button component={ListItem} disableRipple>
+        <Button className="menu-hover-effect" component={ListItem} disableRipple>
           <ListItemText
-            primary={<Typography variant="body1">ABOUT</Typography>}
+            primary={<Typography className="text-menu" variant="body1">ABOUT</Typography>}
           />
         </Button>
       </NavLink>
       <NavLink to="/contact" style={{ textDecoration: "none" }}>
-        <Button component={ListItem} disableRipple>
+        <Button className="menu-hover-effect" component={ListItem} disableRipple>
           <ListItemText
-            primary={<Typography variant="body1">CONTACT</Typography>}
+            primary={<Typography className="text-menu" variant="body1">CONTACT</Typography>}
           />
         </Button>
       </NavLink>
