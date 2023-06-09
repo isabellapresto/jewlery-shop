713acab0c66b95b487bf6ddd68e3d77327eb5b45
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-06-09 15:58:47 +0200

    uppdat

--- client/src/components/AdminProducts/AdminProducts.tsx
+++ client/src/components/AdminProducts/AdminProducts.tsx
@@ -3,24 +3,33 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import { formatCurrency } from "../../utilities/formatCurrency";
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TextField from '@mui/material/TextField';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function AdminProducts() {
+
 
   const {products} = useProductContext();
 
   return (
     <>
-        {products.map((product) => (
+    {products.map((product) => (
+      <Accordion>
+      <AccordionSummary
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
         <Stack direction="row"
         spacing={3}
         alignItems="center"
         marginLeft={'5%'}
-        width={'90%'}
+        width={'100%'}
         justifyContent={'space-between'}
         >
-
         <Box >
-            <img 
+          <img 
             src={product?.image}
             style={{width: '50px', height: '50px', objectFit: 'cover'}}
             />
@@ -30,13 +39,32 @@ export default function AdminProducts() {
             <span className="product-title">{product?.title} {" "}</span>
             
         </Box>
-        <Box style={{width: '10%'}}>
+        <Box style={{width: '5%'}}>
           <span className="product-price ">{product && formatCurrency(product?.price)}</span>
           </Box>
-        <Button variant='outlined'>Modify product</Button>
-        <Button variant='outlined'>Remove product</Button>
-    </Stack>
-        ))}
+          <Button variant='outlined' endIcon={<DeleteForeverIcon />}>Remove product</Button>
+          <Button variant='outlined' endIcon={<ExpandMoreIcon />}>Modify product </Button>
+      </Stack>
+      </AccordionSummary>
+      <AccordionDetails>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '50' }
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField required
+          id="outlined-required" label="Image" variant="outlined" />
+      <TextField required
+          id="outlined-required" label="Name" variant="outlined" />
+      <TextField required
+          id="outlined-required" label="Price" variant="outlined" />
+    </Box>
+      </AccordionDetails>
+    </Accordion>
+    ))}
     </>
-  );
+  )
 }
