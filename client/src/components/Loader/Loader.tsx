2c10b120d944cd5f5f04616ff4881512a07bb422
AjshaC <94913424+AjshaC@users.noreply.github.com>
2023-06-12 14:58:31 +0200

    Loader Correct Size

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,11 +1,20 @@
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 export default function CircularIndeterminate() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <p>Vänligen vänta, vi bearbetar din order</p>
-        <CircularProgress />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60vh",
+      }}
+    >
+      <br />
+      <p>Please wait while we process your order!</p>
+      <CircularProgress />
     </Box>
   );
 }
