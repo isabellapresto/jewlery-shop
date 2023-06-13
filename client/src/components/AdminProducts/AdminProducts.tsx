4ab4457413762457791aeaceafc7e3169cf25612
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-06-13 19:25:30 +0200

    Function deleteProductFromDatabase

--- client/src/components/AdminProducts/AdminProducts.tsx
+++ client/src/components/AdminProducts/AdminProducts.tsx
@@ -1,13 +1,46 @@
+//import { useEffect, useState } from "react";
+//import { useNavigate } from "react-router-dom";
 import { useProductContext } from "../../context/ProductContext";
+//import { Product } from "../../context/ProductContext";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import { formatCurrency } from "../../utilities/formatCurrency";
 
+
 export default function AdminProducts() {
 
   const {products} = useProductContext();
 
+  //----------------------------START - Deleting product from database-------------------------------------//
+
+  const deleteProductFromDatabase = (id: string) => {
+
+    const url = 'api/products/' + id;
+  
+    fetch(url, {method: "DELETE"})
+      .then((response) => {
+        if (!response){
+          throw new Error("Something went wrong, the product with " + id + " is not deleted");
+        }
+        console.log("Product with id " + id + " is now deleted from database")
+        //RENDER ALL PRODUCTS AGAIN
+      })
+      
+    .catch ((e) => {
+      console.log(e);
+    });
+    }
+  
+    //Eventlistener on delete button
+      const handleDelete = async (event: React.MouseEvent<HTMLElement>, id:string) => {
+        event.preventDefault();
+        deleteProductFromDatabase(id);
+      }
+  
+  //----------------------------END - Deleting product from database-------------------------------------//
+
+ 
   return (
     <>
         {products.map((product) => (
@@ -19,7 +52,7 @@ export default function AdminProducts() {
         justifyContent={'space-between'}
         >
 
-        <Box >
+        <Box>
             <img 
             src={product?.image}
             style={{width: '50px', height: '50px', objectFit: 'cover'}}
@@ -28,13 +61,16 @@ export default function AdminProducts() {
 
         <Box style={{width: '40%'}}>
             <span className="product-title">{product?.title} {" "}</span>
-            
         </Box>
+
         <Box style={{width: '10%'}}>
           <span className="product-price ">{product && formatCurrency(product?.price)}</span>
-          </Box>
+        </Box>
+
         <Button variant='outlined'>Modify product</Button>
-        <Button variant='outlined'>Remove product</Button>
+
+        <Button variant='outlined' type="submit" onClick={(e) => handleDelete(e, product._id)}>Delete</Button>
+
     </Stack>
         ))}
     </>
