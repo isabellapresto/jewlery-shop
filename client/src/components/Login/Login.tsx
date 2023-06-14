0c142942474cf9f9bc94775b87da3b795ead174b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-06-14 22:56:32 +0200

    cleaning

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,20 +1,12 @@
-// import React from 'react';
 import LoginForm from "../LoginForm/LoginForm"
 import "./Login.css"
-// import { CurrentUserContext} from '../CurrentUserContext';
-// import {Drawer, Button} from "@mui/material";
 
 function Login() {
-// const { testData } = React.useContext(CurrentUserContext);
 
     return(
-        <div  className="loginContainer">
-            
+        <div className="loginContainer">
             <LoginForm />
-            {/* <h2>{testData}</h2> */}
         </div>
-       
-        
     )
     
 }
