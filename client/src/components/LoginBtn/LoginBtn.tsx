90c55144ff2157dd23c1c3378be7089fea2b27b9
isabellapresto <isabella.presto@gmail.com>
2023-06-14 11:48:32 +0200

    Logout-alert

--- client/src/components/LoginBtn/LoginBtn.tsx
+++ client/src/components/LoginBtn/LoginBtn.tsx
@@ -1,48 +1,56 @@
-import { Button } from '@mui/material';
+import { Button, Popover, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
 import LoginIcon from '@mui/icons-material/LogIn';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { useContext} from 'react'
-import {  UserContextType,} from '../../context/CurrentUserContext'
+import { useContext, useState } from 'react';
+import { UserContextType } from '../../context/CurrentUserContext';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import './LoginBtn.css'
-
-
+import './LoginBtn.css';
 
 export default function LoginBtn() {
-  
-
-  const {logout, loggedInUser } = useContext(UserContextType);
-  
+  const { logout, loggedInUser } = useContext(UserContextType);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
   const handleLogout = async () => {
-    
-    
     await logout();
-    
-   
-      alert('You are logged out ');
-     
-    
-    
+    setPopoverOpen(true);
   };
 
-  
+  const handlePopoverClose = () => {
+    setPopoverOpen(false);
+  };
+
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
   return (
     <>
       {loggedInUser?.isAdmin === false ? (
         <div className='LoginBtnContainer'>
-        <Button size="small" variant="text" startIcon={<LogoutIcon />} onClick={handleLogout}>
-          Logout
-        </Button>
+          <Button size="small" variant="text" startIcon={<LogoutIcon />} onClick={handleLogout}>
+            Logout
+          </Button>
 
-        <Link to="/login">
-          <AccountCircleIcon/>
-        </Link>
-        
+          <Link to="/login">
+            <AccountCircleIcon/>
+          </Link>
+
+          <Popover
+            open={popoverOpen}
+            anchorEl={anchorEl}
+            onClose={handlePopoverClose}
+            anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+            }}
+          >
+            <Typography sx={{ p: 2 }}>You are logged out</Typography>
+          </Popover>
         </div>
-      ) :  loggedInUser ? (
+      ) : loggedInUser ? (
         <div className='LoginBtnContainer'>
           <Link to="/">
             <Button variant="text" startIcon={<LogoutIcon />} onClick={handleLogout}>
@@ -57,20 +65,40 @@ export default function LoginBtn() {
           <Link to="/admin">
             <AdminPanelSettingsIcon/>
           </Link>
-        
-        </div>
 
-        ) : (
+          <Popover
+            open={popoverOpen}
+            anchorEl={anchorEl}
+            onClose={handlePopoverClose}
+            anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+            }}
+          >
+            <Typography sx={{ p: 2 }}>You are logged out</Typography>
+          </Popover>
+        </div>
+      ) : (
         <div className='LoginBtnContainer'>
           <Link to="/login">
             <Button variant="text" startIcon={<LoginIcon />}>
               LogIn
             </Button>
           </Link>
+
+          <Popover
+            open={popoverOpen}
+            anchorEl={anchorEl}
+            onClose={handlePopoverClose}
+            anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+            }}
+          >
+            <Typography sx={{ p: 2 }}>You are logged out</Typography>
+          </Popover>
         </div>
-        
-       )}
+      )}
     </>
-     
-  )
+  );
 }
