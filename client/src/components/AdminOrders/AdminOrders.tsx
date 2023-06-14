79ea96af6bd3317031820637abbef78a35fddf6f
Elin Forsberg <elinfsp96@hotmail.com>
2023-06-14 16:26:41 +0200

    admin orders done

--- client/src/components/AdminOrders/AdminOrders.tsx
+++ client/src/components/AdminOrders/AdminOrders.tsx
@@ -3,6 +3,11 @@ import {useEffect, useState} from 'react'
 import { CartItem } from "../../context/CartContext";
 import { Address } from "../../context/OrderContext";
 import Button from '@mui/material/Button';
+import { User } from '../../context/CurrentUserContext';
+import { NavLink } from 'react-router-dom';
+import {  Typography,  } from '@mui/material';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
 
 export interface ShippedOrder {
     orderItems: CartItem[];
@@ -10,7 +15,7 @@ export interface ShippedOrder {
     shippingMethod: string | number;
     orderNumber?: number;
     shipped: boolean;
-    customer: string;
+    customer: User;
     _id: string;
     
   }
@@ -19,7 +24,7 @@ export interface ShippedOrder {
 function AdminOrders() {
 
     const [ orders, setOrders ] = useState<ShippedOrder[]>([]);
-    // const [isShipped, setIsShipped] = useState<false | boolean>();
+    
     const getShippingMethods = async () => {
         try {
           const response = await fetch(
@@ -27,9 +32,7 @@ function AdminOrders() {
           );
           const data = await response.json();
           setOrders(data);
-          // setIsShipped(true)
-          console.log(data);
-          // console.log(shipping);
+         
         } catch (err) {
           console.log(err);
         }
@@ -40,10 +43,8 @@ function AdminOrders() {
         getShippingMethods();
       }, []);
 
-    //   const { id } = useParams();
       const [newOrder, setNewOrder] = useState("")
 
-    //   useEffect (() => {
         const markAsShipped = async (id: string) => {
             
                 try{
@@ -59,17 +60,10 @@ function AdminOrders() {
                     const data = await response.json();
                     
                     if (response.status === 200) {
-                        
-                        // setIsShipped(true)
-                        // if(_id === id) {
+                       
                             setNewOrder(data)
                             getShippingMethods();
-                            console.log("marked as shipped");
-                        // }
-                        
-                        
-                        // setOrders(data) 
-                        // console.log("order marked as shipped");
+                          
                       } else {
                         console.log("sorry, the order is not marked as shipped");
                       }
@@ -78,38 +72,65 @@ function AdminOrders() {
     }
 
 }
-        // markAsShipped()
-    //   },[id]);
-        
-
+      
       const handleSubmit = async (event: React.MouseEvent<HTMLElement>, id:string) => {
         event.preventDefault();
         markAsShipped(id);
-        // console.log(isShipped);
-        
-        
-        
+       
       }
-   
+      
+return (
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+         <div style={{width: '5rem', marginLeft: '30%', paddingTop: '30px', paddingBottom: '30px'}}>
+        <NavLink to="/admin" style={{textDecoration: "none" }}>
+          <Button variant='outlined'>Back</Button>
+        </NavLink>
+      </div>
+      <div style={{width:"80vw"}}>
+      <Typography variant="h4" component="h1" gutterBottom fontFamily={'Cormorant Garamond, serif'} fontWeight={500} style= {{textAlign:"center", paddingBottom:20}}>
+          Admin Orders
+        </Typography>
 
-  return (
-    
-        <div>
-          <h2>Admin Orders</h2>
-          {orders.map((order) => (
-            <div key={order.orderNumber}>
-                <p style={{paddingTop:20}}>Ordernumber: {order.orderNumber}</p>
-                <p>{order.customer.firstName + " " + order.customer.lastName}</p>
-                <p>{order.deliveryAddress.street + " " + order.deliveryAddress.zipcode + " " + order.deliveryAddress.city  + " " + order.deliveryAddress.country}</p>
-                <Button type="submit" onClick={(e) => handleSubmit(e, order._id)}>mark as shipped</Button>
-              
-             </div>
+
+      {orders.map((order) => (
+        <div key={order.orderNumber} style={{display:"flex", flexDirection:"row", justifyContent:"space-between", borderBottom: "2px solid  lightgrey" }}>
            
-          ))}
+  
+            <div >
+            <div style={{paddingBottom:20, paddingTop:20}}>
+            <p style={{ fontWeight:"bold", paddingBottom:7 }}>Ordernumber: {order.orderNumber}</p>
+          <p>Name: {order.customer.firstName} {order.customer.lastName}</p>
+          <p>Address: {order.deliveryAddress.street} {order.deliveryAddress.zipcode}{" "}
+            {order.deliveryAddress.city} {order.deliveryAddress.country}
+          </p>
+            </div>
+            
+            </div>
+            <div style={{textAlign:"center"}}>
+            {order.shipped ? (
+                <div style={{color:"green", paddingTop:28}}>
+                    <CheckCircleOutlineIcon/>
+                    <p>Order is shipped</p>
+                </div>
+            
+          ) : (
+            <div>
+                <Button type="submit" onClick={(e) => handleSubmit(e, order._id)} style={{paddingTop:40}}>
+              Mark as shipped
+            </Button>
+            </div>
+            
+          )}
+            </div>
+          
+          
         </div>
-      );
-    }
-    
+      ))}
+      </div>
+      
+    </div>
+  );
+}
 
 export default AdminOrders
 
